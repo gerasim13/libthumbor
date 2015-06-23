f7d0f002f706de5cbe13e393abda9d177217088c
Павел Литвиненко <gerasim13@gmail.com>
2015-06-23 13:57:48 +0300

    no message

--- libthumbor/flask/field.py
+++ libthumbor/flask/field.py
@@ -41,4 +41,5 @@ class ThumborField(BaseField):
         return
 
     def to_python(self, value):
-        return ThumborData({ 'path' : value })
+        dict = { 'path' : value }
+        return ThumborData(**dict)
