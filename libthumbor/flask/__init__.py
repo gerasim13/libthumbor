487d6f3f128ea955c34d50634305e8eba9daf9c5
Павел Литвиненко <gerasim13@gmail.com>
2015-07-30 10:52:05 +0300

    no message

--- libthumbor/flask/__init__.py
+++ libthumbor/flask/__init__.py
@@ -1,4 +1,5 @@
+from libthumbor.flask.converter import BackendModelConverter
 from libthumbor.flask.field     import ThumborField, ThumborData
 from libthumbor.flask.views     import ThumborImageInput, ThumborImageField
-from libthumbor.flask.converter import BackendModelConverter
+from libthumbor.flask.views     import THUMBOR_FORMATTERS
 __all__ = ['field', 'views', 'converter']
